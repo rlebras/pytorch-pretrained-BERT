8055c6b6c88e894a486deb6c10f5c7085db12374
chandrab <chandrab@allenai.org>
2018-11-19 19:55:49 -0800

    Multiple choice features

--- examples/run_classifier.py
+++ examples/run_classifier.py
@@ -62,6 +62,30 @@ class InputExample(object):
         self.label = label
 
 
+class InputExampleWithList(object):
+    """A single training/test example for simple multiple choice classification."""
+
+    def __init__(self, guid, text_a, text_b, label=None):
+        """Constructs a InputExample.
+
+        Args:
+            guid: Unique id for the example.
+            text_a: list. A list containing untokenized text
+            text_b: list. containing untokenized text associated of the same size as text_A
+            Only must be specified for multiple choice options.
+            label: (Optional) string. The label of the example. This should be
+            specified for train and dev examples, but not for test examples.
+        """
+        assert isinstance(text_a, list)
+        assert isinstance(text_b, list)
+        assert len(text_a) == len(text_b)
+
+        self.guid = guid
+        self.text_a = text_a
+        self.text_b = text_b
+        self.label = label
+
+
 class InputFeatures(object):
     """A single set of features of data."""
 
@@ -147,10 +171,12 @@ class AnliProcessor(DataProcessor):
 
             label = convert_to_unicode(str(answer))
             examples.append(
-                InputExample(guid=guid, text_a=option1_context, text_b=ending, label=label))
-            examples.append(
-                InputExample(guid=guid, text_a=option2_context, text_b=ending, label=label))
-
+                InputExampleWithList(guid=guid,
+                                     text_a=[option1_context, option2_context],
+                                     text_b=[ending, ending],
+                                     label=label
+                                     )
+            )
         return examples
 
 
@@ -342,6 +368,111 @@ def convert_examples_to_features(examples, label_list, max_seq_length, tokenizer
                               input_mask=input_mask,
                               segment_ids=segment_ids,
                               label_id=label_id))
+    return features
+
+
+def convert_examples_to_features_mc(examples, label_list, max_seq_length, tokenizer):
+    """Loads a data file into a list of `InputBatch`s."""
+
+    label_map = {}
+    for (i, label) in enumerate(label_list):
+        label_map[label] = i
+
+    features = []
+    for (ex_index, example) in enumerate(examples):
+        tokens_a = tokenizer.tokenize(example.text_a)
+
+        tokens_b = None
+        if example.text_b:
+            tokens_b = tokenizer.tokenize(example.text_b)
+
+        if tokens_b:
+            # Modifies `tokens_a` and `tokens_b` in place so that the total
+            # length is less than the specified length.
+            # Account for [CLS], [SEP], [SEP] with "- 3"
+            _truncate_seq_pair(tokens_a, tokens_b, max_seq_length - 3)
+        else:
+            # Account for [CLS] and [SEP] with "- 2"
+            if len(tokens_a) > max_seq_length - 2:
+                tokens_a = tokens_a[0:(max_seq_length - 2)]
+
+        # The convention in BERT is:
+        # (a) For sequence pairs:
+        #  tokens:   [CLS] is this jack ##son ##ville ? [SEP] no it is not . [SEP]
+        #  type_ids: 0   0  0    0    0     0       0 0    1  1  1  1   1 1
+        # (b) For single sequences:
+        #  tokens:   [CLS] the dog is hairy . [SEP]
+        #  type_ids: 0   0   0   0  0     0 0
+        #
+        # Where "type_ids" are used to indicate whether this is the first
+        # sequence or the second sequence. The embedding vectors for `type=0` and
+        # `type=1` were learned during pre-training and are added to the wordpiece
+        # embedding vector (and position vector). This is not *strictly* necessary
+        # since the [SEP] token unambigiously separates the sequences, but it makes
+        # it easier for the model to learn the concept of sequences.
+        #
+        # For classification tasks, the first vector (corresponding to [CLS]) is
+        # used as as the "sentence vector". Note that this only makes sense because
+        # the entire model is fine-tuned.
+        all_tokens = []
+        all_token_ids = []
+        all_segments = []
+        all_masks = []
+        for a, b in zip(tokens_a, tokens_b):
+            tokens = []
+            segment_ids = []
+            tokens.append("[CLS]")
+            segment_ids.append(0)
+            for token in a:
+                tokens.append(token)
+                segment_ids.append(0)
+            tokens.append("[SEP]")
+            segment_ids.append(0)
+
+            for token in b:
+                tokens.append(token)
+                segment_ids.append(1)
+            tokens.append("[SEP]")
+            segment_ids.append(1)
+
+            input_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+            # The mask has 1 for real tokens and 0 for padding tokens. Only real
+            # tokens are attended to.
+            input_mask = [1] * len(input_ids)
+
+            # Zero-pad up to the sequence length.
+            while len(input_ids) < max_seq_length:
+                input_ids.append(0)
+                input_mask.append(0)
+                segment_ids.append(0)
+
+            assert len(input_ids) == max_seq_length
+            assert len(input_mask) == max_seq_length
+            assert len(segment_ids) == max_seq_length
+
+            all_tokens.append(tokens)
+            all_token_ids.append(input_ids)
+            all_segments.append(segment_ids)
+            all_masks.append(input_mask)
+
+        label_id = label_map[example.label]
+        if ex_index < 5:
+            logger.info("*** Example ***")
+            logger.info("guid: %s" % (example.guid))
+            logger.info("tokens: %s" % " ".join(
+                [printable_text(x) for x in all_tokens[0]]))
+            logger.info("input_ids: %s" % " ".join([str(x) for x in all_token_ids[0]]))
+            logger.info("input_mask: %s" % " ".join([str(x) for x in all_masks[0]]))
+            logger.info(
+                "segment_ids: %s" % " ".join([str(x) for x in all_segments[0]]))
+            logger.info("label: %s (id = %d)" % (example.label, label_id))
+
+        features.append(
+            InputFeatures(input_ids=all_token_ids,
+                          input_mask=all_masks,
+                          segment_ids=all_segments,
+                          label_id=label_id))
     return features
 
 
@@ -574,8 +705,12 @@ def main():
 
     global_step = 0
     if args.do_train:
-        train_features = convert_examples_to_features(
-            train_examples, label_list, args.max_seq_length, tokenizer)
+        if task_name.lower() == "anli":
+            train_features = convert_examples_to_features_mc(
+                train_examples, label_list, args.max_seq_length, tokenizer)
+        else:
+            train_features = convert_examples_to_features(
+                train_examples, label_list, args.max_seq_length, tokenizer)
         logger.info("***** Running training *****")
         logger.info("  Num examples = %d", len(train_examples))
         logger.info("  Batch size = %d", args.train_batch_size)
@@ -635,8 +770,12 @@ def main():
 
     if args.do_eval:
         eval_examples = processor.get_dev_examples(args.data_dir)
-        eval_features = convert_examples_to_features(
-            eval_examples, label_list, args.max_seq_length, tokenizer)
+        if task_name == "anli":
+            eval_features = convert_examples_to_features_mc(
+                eval_examples, label_list, args.max_seq_length, tokenizer)
+        else:
+            eval_features = convert_examples_to_features(
+                eval_examples, label_list, args.max_seq_length, tokenizer)
         logger.info("***** Running evaluation *****")
         logger.info("  Num examples = %d", len(eval_examples))
         logger.info("  Batch size = %d", args.eval_batch_size)
